db078d2c0bc25e43f1f813fc12e849f06b699860
Woojin J. Lim <isp_lwj@naver.com>
2018-08-10 22:58:45 +0900

    Added Some codes

--- Lean_System/MainWindow.xaml.cs
+++ Lean_System/MainWindow.xaml.cs
@@ -1,34 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 
 namespace Lean_System
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public void FileChanged(String name)
+        public void saveFile()
+        {
+            var asdf = new Person();
+            var json = JsonConvert.SerializeObject(asdf);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine("C:\\Users\\Woojin\\Desktop", "test.json")))
+            {
+                    outputFile.WriteLine(json);
+            }
+        }
+
+        public void openFile()
+        {
+            string test = "";
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+                test = File.ReadAllText(openFileDialog.FileName);
+
+            try
+            {
+                Person jsonPerson = JsonConvert.DeserializeObject<Person>(test);
+                MessageBox.Show(jsonPerson.getName());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void fileChanged(string name)
         {
             currentFileName.Text = name;
         }
+
         public MainWindow()
         {
             InitializeComponent();
-            EPL.Click += new RoutedEventHandler((object sender, RoutedEventArgs e) => (new PeopleList()).ShowDialog());
-            FileChanged("asdf");
+            EPL.Click += (sender, e) => new PeopleList().ShowDialog();
+            OF.Click += (sender, e) => openFile();
+            SF.Click += (sender, e) => saveFile();
+            fileChanged("asdf");
         }
     }
 }
---
+++ Lean_System/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Lean_System
+{
+    enum POSITION
+    {
+        WORKER, ASDF
+    }
+
+    [Serializable]
+    class Person : ISerializable
+    {
+        public Person()
+        {
+            this.positions.Add(POSITION.WORKER);
+            this.positions.Add(POSITION.ASDF);
+            this.positions.Add(POSITION.ASDF);
+        }
+
+        public string getName()
+        {
+            MessageBox.Show((POSITION.WORKER == 0).ToString());
+            return this.name;
+        }
+
+        protected Person(SerializationInfo info, StreamingContext context)
+        {
+            this.name = info.GetString("Name");
+            this.positions = (List<POSITION>)info.GetValue("Position", typeof(List<POSITION>));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", this.name);
+            info.AddValue("Position", this.positions);
+        }
+
+        private List<POSITION> positions = new List<POSITION>();
+        private string name = "ASDF";
+    }
+
+}
