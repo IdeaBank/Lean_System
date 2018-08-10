da54b23f1b69cd9216ffa3acaac38e98e92b3b41
Woojin J. Lim <isp_lwj@naver.com>
2018-08-10 10:13:11 +0900

    Implemented Basic UI

---
+++ Lean_System/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lean_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public void FileChanged(String name)
+        {
+            currentFileName.Text = name;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            EPL.Click += new RoutedEventHandler((object sender, RoutedEventArgs e) => (new PeopleList()).ShowDialog());
+            FileChanged("asdf");
+        }
+    }
+}
